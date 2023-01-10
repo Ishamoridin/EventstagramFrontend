59e25e1ecf052830aee3e996952d642547db214c
Ishamoridin <Ishamoridin@googlemail.com>
2023-01-10 16:46:09 +0000

    API fetchString builder

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,12 @@
-export async function UpdateMap() {};
-export async function UpdateWeather() {};
+require('dotenv')
+// export async function updateMap() {};
+// export async function updateWeather() {};
+export async function getGeolocation(location) {
+    const fetchString = process.env.REACT_APP_BING_MAP_STRINGSTART+'?query='+location+process.env.REACT_APP_BING_MAP_KEYSTRING;
+    try {
+        const results = await fetch(fetchString);
+        console.log(results)
+    } catch (error) {
+        console.log(error)
+    }
+};
