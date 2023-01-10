45ddb339f5153141a603689f47d6d30e420d0dc5
Ishamoridin <Ishamoridin@googlemail.com>
2023-01-10 16:54:21 +0000

    .

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 require('dotenv')
 // export async function updateMap() {};
 // export async function updateWeather() {};
-export async function getGeolocation(location) {
+export async function getGeolocationFromLocationString(location) {
     const fetchString = process.env.REACT_APP_BING_MAP_STRINGSTART+'?query='+location+process.env.REACT_APP_BING_MAP_KEYSTRING;
     try {
         const results = await fetch(fetchString);
