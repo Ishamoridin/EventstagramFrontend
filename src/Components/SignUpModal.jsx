9c7f75783bae1c6d611fea454e2d38e21cb57c4b
Aerdonix <badpixel71@gmail.com>
2023-01-16 15:40:48 +0000

    Added redirect/navigate to SignUpModal & added p tag for password definition.

--- src/Components/SignUpModal.jsx
+++ src/Components/SignUpModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { createUser } from "../utils";
 import "../styles/LoginModal.css";
@@ -8,17 +8,20 @@ const SignUpModal = () => {
     const [username, setUsername] = useState()
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
+    const navigate = useNavigate();
 
     const submitHandler = async (event) => {
-        event.preventDefault()
-        console.log(username)
-        console.log(email)
-        console.log(password)
-        await createUser(username, email, password)
-    }
-
+        event.preventDefault();
+        console.log(username);
+        console.log(email);
+        console.log(password);
+        const status = await createUser(username, email, password);
+        if (!loggedInUser){tryToLogin(true)}else{navigate('/')};
+    };
+    const [triedToLogin, tryToLogin] = useState(false)
     return (
         <div className="outerContainer">
+             <p className="pleaseTryAgain" style={{visibility: triedToLogin ? 'visible' : 'hidden' }}>Please Try Again</p>
             <div className="innerContainer">
                 <form onSubmit={submitHandler}>
                     <label>Username:
@@ -31,6 +34,7 @@ const SignUpModal = () => {
                     <br/>
                     <label>Password:
                         <input type="password" onChange={(event) => setPassword(event.target.value) } required />
+                        <p>Password requires at least 8 characters, A Capital, A Number and A Non Alphanumeric Character.</p>
                     </label>
                     <br/>
                     <button type="submit" className="login">Create Account</button>
