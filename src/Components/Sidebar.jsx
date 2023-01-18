8f0cd8ee0b3c73ab2b16fad227d56359a3277137
Max Cowen-Carr <maxacowencarr@icloud.com>
2023-01-18 17:08:30 +0000

    home and card style

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,23 +3,48 @@ import { VscHome } from "react-icons/vsc";
 import { MdEvent } from "react-icons/md";
 import { MdOutlineCreate } from "react-icons/md";
 import { CiUser } from "react-icons/ci";
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
-
-
-
-const Sidebar = ({sidebar, logUserIn}) => {
+const Sidebar = ({ sidebar, logUserIn }) => {
   return (
-    <div className={sidebar?"sidebar sidebar--open":"sidebar"}>
-        <li><i><VscHome/></i><Link to="/">Home</Link></li>
-        <li><i><MdEvent/></i><Link to="/EventPage">EventPage</Link></li>
-        <li><i><MdOutlineCreate/></i><Link to="/PostEvent">PostEvent</Link></li>
-        <li><i><CiUser/></i><Link to="/UserProfile">Profile</Link></li>
-        <li><i><CiUser/></i><Link onClick={() => logUserIn(null)} to="/login">Logout</Link></li>
-        </div>
-  )
-
+    <div className={sidebar ? "sidebar sidebar--open" : "sidebar"}>
+      <div className="list-wrapper">
+        <li className="sidebar-list-item">
+          <i>
+            <VscHome />
+          </i>
+          <Link to="/">Home</Link>
+        </li>
+        <li className="sidebar-list-item">
+          <i>
+            <MdEvent />
+          </i>
+          <Link to="/EventPage">EventPage</Link>
+        </li>
+        <li className="sidebar-list-item">
+          <i>
+            <MdOutlineCreate />
+          </i>
+          <Link to="/PostEvent">Post</Link>
+        </li>
+        <li className="sidebar-list-item">
+          <i>
+            <CiUser />
+          </i>
+          <Link to="/UserProfile">Profile</Link>
+        </li>
+        <li className="sidebar-list-item">
+          <i>
+            <CiUser />
+          </i>
+          <Link onClick={() => logUserIn(null)} to="/login">
+            Logout
+          </Link>
+        </li>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
