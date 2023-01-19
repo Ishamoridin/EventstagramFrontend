698f8c107bf2b4f5e7714b4a0fdb521041c82f08
Ishamoridin <Ishamoridin@googlemail.com>
2023-01-19 14:08:41 +0000

    Update Sidebar.jsx

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { VscHome } from "react-icons/vsc";
-// import { MdEvent } from "react-icons/md";
 import { MdOutlineCreate } from "react-icons/md";
 import { CiUser } from "react-icons/ci";
 import { Link } from "react-router-dom";
@@ -13,7 +12,6 @@ const Sidebar = ({sidebar, setUser}) => {
     return (
     <div className={sidebar?"sidebar sidebar--open":"sidebar"}>
         <li><i><VscHome/></i><Link to="/">Home</Link></li>
- //       <li><i><MdEvent/></i><Link to="/EventPage">EventPage</Link></li>
         <li><i><MdOutlineCreate/></i><Link to="/PostEvent">PostEvent</Link></li>
         <li><i><CiUser/></i><Link to="/UserProfile">Profile</Link></li>
         <li><i><CiUser/></i><Link onClick={() => setUser(null)} to="/login">Logout</Link></li>
