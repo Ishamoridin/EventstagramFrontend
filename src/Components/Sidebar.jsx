9ca290ea2d86540e0a7ecaa0a7aaa3b10e7bc402
Ishamoridin <Ishamoridin@googlemail.com>
2023-01-19 14:06:58 +0000

    Update Sidebar.jsx
    Commented out EventPage link

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({sidebar, setUser}) => {
     return (
     <div className={sidebar?"sidebar sidebar--open":"sidebar"}>
         <li><i><VscHome/></i><Link to="/">Home</Link></li>
-        <li><i><MdEvent/></i><Link to="/EventPage">EventPage</Link></li>
+ //       <li><i><MdEvent/></i><Link to="/EventPage">EventPage</Link></li>
         <li><i><MdOutlineCreate/></i><Link to="/PostEvent">PostEvent</Link></li>
         <li><i><CiUser/></i><Link to="/UserProfile">Profile</Link></li>
         <li><i><CiUser/></i><Link onClick={() => setUser(null)} to="/login">Logout</Link></li>
