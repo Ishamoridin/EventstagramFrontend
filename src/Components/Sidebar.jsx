974533da836befd57e6667e37d1238f8713c316f
Ishamoridin <Ishamoridin@googlemail.com>
2023-01-19 20:21:55 +0000

    removed Home from sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -11,7 +11,7 @@ import "../styles/Navbar.css";
 const Sidebar = ({sidebar, setUser, bigUser}) => {
     return (
     <div className={sidebar?"sidebar sidebar--open":"sidebar"}>
-        <li style={{ visibility: bigUser ? "visible" : "hidden" }}><i><VscHome/></i><Link to="/Home">Home</Link></li>
+        {/* <li style={{ visibility: bigUser ? "visible" : "hidden" }}><i><VscHome/></i><Link to="/Home">Home</Link></li> */}
         <li style={{ visibility: bigUser ? "visible" : "hidden" }}><i><MdOutlineCreate/></i><Link to="/PostEvent">PostEvent</Link></li>
         <li style={{ visibility: bigUser ? "visible" : "hidden" }}><i><CiUser/></i><Link to="/UserProfile">Profile</Link></li>
         <li><i><CiUser/></i><Link onClick={() => setUser(null)} to="/">{bigUser ? "Logout" : "Login"}</Link></li>
