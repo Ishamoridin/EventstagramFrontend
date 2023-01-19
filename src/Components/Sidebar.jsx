58f8290406d3d7ef64228b38d370e3334ae94692
Ishamoridin <Ishamoridin@googlemail.com>
2023-01-19 17:56:00 +0000

    .

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = ({sidebar, setUser}) => {
         <li><i><VscHome/></i><Link to="/">Home</Link></li>
         <li><i><MdOutlineCreate/></i><Link to="/PostEvent">PostEvent</Link></li>
         <li><i><CiUser/></i><Link to="/UserProfile">Profile</Link></li>
-        <li><i><CiUser/></i><Link onClick={() => setUser(null)} to="/login">Logout</Link></li>
+        <li><i><CiUser/></i><Link onClick={() => setUser(null)} to="/">Logout</Link></li>
         </div>
   )
 
